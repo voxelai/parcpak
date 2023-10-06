91bf8b36bf047804b001af9497fa125cffe2466d
Dan Gale <danjgale@gmail.com>
2023-10-06 16:48:17 -0600

    added in documentation and overwriting/download_dir options

--- parcpak/main.py
+++ parcpak/main.py
@@ -17,6 +17,18 @@ def _download_file(
     download_dir: str | Path | None = None,
     overwrite: bool = False,
 ):
+    """Download file from URL
+
+    Args:
+        url (str): File URL
+        download_dir (str | Path | None, optional): Directory to save file in.
+        If None, default directory ($HOME/parcpak) is used. Defaults to None.
+        overwrite (bool, optional): If False, download is skipped if file
+        exists; if True, then overwrite existing file. Defaults to False.
+
+    Returns:
+        str: Path to downloaded file
+    """
     if download_dir is None:
         download_dir = PARCPAK_DIR
     os.makedirs(download_dir, exist_ok=True)
@@ -31,7 +43,25 @@ def _download_file(
     return str(target_file)
 
 
-def fetch_schaefer(version: int, resolution: int = 2):
+def fetch_schaefer(
+    version: int,
+    resolution: int = 2,
+    download_dir: str | Path | None = None,
+    overwrite: bool = False,
+) -> tuple:
+    """Get specific Schaefer parcellation
+
+    Args:
+        version (int): Number of regions in Schaefer parcellation (100-1000)
+        resolution (int, optional): 1 or 2 mm version of parcellation. Defaults to 2.
+        download_dir (str | Path | None, optional): Directory to save file in.
+        If None, default directory ($HOME/parcpak) is used. Defaults to None.
+        overwrite (bool, optional): If False, download is skipped if file
+        exists; if True, then overwrite existing file. Defaults to False.
+
+    Returns:
+        tuple: (Parcellation name, path to file, path to associated label table)
+    """
     name = f"Schaefer{version}_7Networks"
     parc = _download_file(
         os.path.join(
@@ -39,6 +69,8 @@ def fetch_schaefer(version: int, resolution: int = 2):
             "parcellations",
             f"Schaefer2018_{version}Parcels_7Networks_order_FSLMNI152_{resolution}mm.nii.gz",
         ),
+        download_dir,
+        overwrite,
     )
 
     table = _download_file(
@@ -47,12 +79,30 @@ def fetch_schaefer(version: int, resolution: int = 2):
             "tables",
             f"Schaefer2018_{version}Parcels_7Networks.csv",
         ),
+        download_dir,
+        overwrite,
     )
 
     return name, parc, table
 
 
-def fetch_diedrichsen(resolution: int = 2):
+def fetch_diedrichsen(
+    resolution: int = 2,
+    download_dir: str | Path | None = None,
+    overwrite: bool = False,
+) -> tuple:
+    """Download Diedrichsen cerebellar parcellation
+
+    Args:
+        resolution (int, optional): 1 or 2 mm version of parcellation. Defaults to 2.
+        download_dir (str | Path | None, optional): Directory to save file in.
+        If None, default directory ($HOME/parcpak) is used. Defaults to None.
+        overwrite (bool, optional): If False, download is skipped if file
+        exists; if True, then overwrite existing file. Defaults to False.
+
+    Returns:
+        tuple: (Parcellation name, path to file, path to associated label table)
+    """
     name = "Diedrichsen"
     parc = _download_file(
         os.path.join(
@@ -60,6 +110,8 @@ def fetch_diedrichsen(resolution: int = 2):
             "parcellations",
             f"Diedrichsen_space-MNI_dseg_{resolution}mm.nii.gz",
         ),
+        download_dir,
+        overwrite,
     )
 
     table = _download_file(os.path.join(REPO_URL, "tables", "Diedrichsen.csv"))
@@ -67,7 +119,23 @@ def fetch_diedrichsen(resolution: int = 2):
     return name, parc, table
 
 
-def fetch_buckner(resolution: int = 2):
+def fetch_buckner(
+    resolution: int = 2,
+    download_dir: str | Path | None = None,
+    overwrite: bool = False,
+) -> tuple:
+    """Download Buckner cerebellar parcellation
+
+    Args:
+        resolution (int, optional): 1 or 2 mm version of parcellation. Defaults to 2.
+        download_dir (str | Path | None, optional): Directory to save file in.
+        If None, default directory ($HOME/parcpak) is used. Defaults to None.
+        overwrite (bool, optional): If False, download is skipped if file
+        exists; if True, then overwrite existing file. Defaults to False.
+
+    Returns:
+        tuple: (Parcellation name, path to file, path to associated label table)
+    """
     name = "Buckner7"
     parc = _download_file(
         os.path.join(
@@ -75,6 +143,8 @@ def fetch_buckner(resolution: int = 2):
             "parcellations",
             f"Buckner7_space-MNI_dseg_{resolution}mm.nii.gz",
         ),
+        download_dir,
+        overwrite,
     )
 
     table = _download_file(os.path.join(REPO_URL, "tables", "Buckner7.csv"))
@@ -82,7 +152,25 @@ def fetch_buckner(resolution: int = 2):
     return name, parc, table
 
 
-def fetch_tian(version: int, resolution: int = 2):
+def fetch_tian(
+    version: int,
+    resolution: int = 2,
+    download_dir: str | Path | None = None,
+    overwrite: bool = False,
+) -> tuple:
+    """Download Tian subcortical parcellation
+
+    Args:
+        version (int): Parcellation version (1-4)
+        resolution (int, optional): 1 or 2 mm version of parcellation. Defaults to 2.
+        download_dir (str | Path | None, optional): Directory to save file in.
+        If None, default directory ($HOME/parcpak) is used. Defaults to None.
+        overwrite (bool, optional): If False, download is skipped if file
+        exists; if True, then overwrite existing file. Defaults to False.
+
+    Returns:
+        tuple: (Parcellation name, path to file, path to associated label table)
+    """
     name = f"Tian_Subcortex_S{version}"
     parc = _download_file(
         os.path.join(
@@ -90,31 +178,62 @@ def fetch_tian(version: int, resolution: int = 2):
             "parcellations",
             f"Tian_Subcortex_S{version}_3T_{resolution}mm.nii.gz",
         ),
+        download_dir,
+        overwrite,
     )
 
     table = _download_file(
         os.path.join(REPO_URL, "tables", f"Tian_Subcortex_S{version}_3T_label.csv"),
+        download_dir,
+        overwrite,
     )
 
     return name, parc, table
 
 
-def get_parcellations(resolution: int = 2):
+def get_parcellations(resolution: int = 2, overwrite: bool = False) -> list:
+    """Get all standard parcellations and their respective label tables
+
+    Args:
+        resolution (int, optional): 1 or 2 mm version of parcellation. Defaults to 2.
+        overwrite (bool, optional): If False, download is skipped if file
+        exists; if True, then overwrite existing file. Defaults to False.
+
+    Returns:
+        list: List of parcellation tuples. For each parcellation:
+        (Parcellation name, path to file, path to associated label table)
+    """
 
     parc_list = []
     for i in range(100, 1100, 100):
-        parc_list.append(fetch_schaefer(i, resolution))
+        parc_list.append(fetch_schaefer(i, resolution, overwrite))
 
     for i in range(1, 4):
-        parc_list.append(fetch_tian(i, resolution))
+        parc_list.append(fetch_tian(i, resolution, overwrite))
 
-    parc_list.append(fetch_diedrichsen(resolution))
-    parc_list.append(fetch_buckner(resolution))
+    parc_list.append(fetch_diedrichsen(resolution, overwrite))
+    parc_list.append(fetch_buckner(resolution, overwrite))
     return parc_list
 
 
 def img2table(img: str | Path | nib.Nifti1Image, metric: str = "mean"):
+    """Convert NIFTI image into data table of regional values for each
+    standard parcellation
 
+    Computes regional values (e.g., mean, standard deviation) for each
+    parcellation and outputs data in a long data frame
+
+    Args:
+        img (str | Path | nib.Nifti1Image): File path or loaded NIFTI image
+        metric (str, optional): Regional metric to use. Must be one of: sum,
+        mean, median, minimum, maximum, variance, standard_deviation. Defaults
+        to "mean".
+
+    Returns:
+        pandas.DataFrame: Single dataframe with columns "Parcellation",
+        "Region", and "Value"; contains regional values for each
+        parcellation
+    """
     parcs = get_parcellations()
 
     list_ = []
